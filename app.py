3286b91650133ca79d28e34abe94b862a5749069
IsmartFelipeRios <felipe.amaral@ismart.org.br>
2025-05-05 15:50:35 -0300

    correcao filtro bd_segmentado

--- app.py
+++ app.py
@@ -293,6 +293,7 @@ if check_password():
     if df_login.query(f'login == "{st.session_state["authenticated_username"]}"')["cargo"].iloc[0] == "coordenação":
         df_coord = df.query('confirmacao_classificacao_coordenacao != "Sim" and confirmacao_classificacao_coordenacao != "Não" and confirmacao_classificacao_orientadora == "Sim" or confirmacao_classificacao_orientadora == "Não"')
         bd_segmentado = bd.query('apoio_registro_final != "Não" and apoio_registro_final != "Sim"')
+        bd_segmentado = bd.query('apoio_registro == "Não" and apoio_registro == "Sim"')
         cidade_login = df_login.query(f'login == "{st.session_state["authenticated_username"]}"')["cidade"].iloc[0]
         bd_segmentado = bd_segmentado.query(f'Cidade == "{cidade_login}"')
         # filtros bd
@@ -311,7 +312,7 @@ if check_password():
         if selecao_orientadora:
             bd_segmentado = bd_segmentado.query(f"Orientadora in {selecao_orientadora}")
         st.divider()
-        ra_nome_bd = bd_segmentado['RA - NOME - FINAL']
+        
         df_coord = df_coord[df_coord['RA'].isin(bd_segmentado['RA'])]
 
         ra_nome = st.selectbox(
