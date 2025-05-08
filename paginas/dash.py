c65d05388ccadd6252baec9506fec82d5e924e39
IsmartFelipeRios <felipe.amaral@ismart.org.br>
2025-05-08 15:07:00 -0300

    comecando dash

--- paginas/dash.py
+++ paginas/dash.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+from datetime import datetime
+from time import sleep
+import pytz
+from app import ler_sheets
+
+fuso_horario = pytz.timezone('America/Sao_Paulo')
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = ler_sheets('registro')
 
 st.success('dash')
+st.dataframe(df)
