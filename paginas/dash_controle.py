97f94d93661c60968e43147deb21a9b3859f4bf4
IsmartFelipeRios <felipe.amaral@ismart.org.br>
2025-05-21 13:48:15 -0300

    teste

--- paginas/dash_controle.py
+++ paginas/dash_controle.py
@@ -10,14 +10,16 @@ bd = df_login = ler_sheets_cache('bd')
 bd['RA'] = bd['RA'].astype(int)
 bd = bd.merge(df[['RA', 'confirmacao_classificacao_orientadora','conclusao_classificacao_final']], how='left', on='RA')
 
+lista_orientadoras = bd['Orientadora'].unique().tolist()
 
 #visualização
 st.title('Controle por Orientadora')
-st.subheader('Bianca_Marinheiro')
-alunos_orientadora_total = bd.query(f"Orientadora == 'Bianca_Marinheiro'")
-alunos_orientadora_total_registrados = alunos_orientadora_total.query("confirmacao_classificacao_orientadora == 'Não' or confirmacao_classificacao_orientadora == 'Sim'")
-try:
-    st.progress(alunos_orientadora_total_registrados.shape[0]/alunos_orientadora_total.shape[0], f'Você registrou: **{alunos_orientadora_total_registrados.shape[0]}/{alunos_orientadora_total.shape[0]}**')
-except ZeroDivisionError:
-    st.error('Zero Resultados')
+for i in lista_orientadoras:
+    st.subheader(f'{i}')
+    alunos_orientadora_total = bd.query(f"Orientadora == '{i}'")
+    alunos_orientadora_total_registrados = alunos_orientadora_total.query("confirmacao_classificacao_orientadora == 'Não' or confirmacao_classificacao_orientadora == 'Sim'")
+    try:
+        st.progress(alunos_orientadora_total_registrados.shape[0]/alunos_orientadora_total.shape[0], f'Você registrou: **{alunos_orientadora_total_registrados.shape[0]}/{alunos_orientadora_total.shape[0]}**')
+    except ZeroDivisionError:
+        st.error('Zero Resultados')
 
