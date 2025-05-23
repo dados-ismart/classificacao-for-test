9f29a5092bd395c91c4fd9553dec03e1b0816f43
IsmartFelipeRios <felipe.amaral@ismart.org.br>
2025-05-23 14:12:43 -0300

    adicionando .strip

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ def check_password():
             st.session_state["password_correct"] = False
 
     if "password_correct" not in st.session_state:
-        st.text_input("Usuário", key="username")
-        st.text_input("Senha", type="password", key="password", on_change=password_entered)
+        st.text_input("Usuário", key="username").strip()
+        st.text_input("Senha", type="password", key="password", on_change=password_entered).strip()
         return False
     elif not st.session_state["password_correct"]:
-        st.text_input("Usuário", key="username")
-        st.text_input("Senha", type="password", key="password", on_change=password_entered)
+        st.text_input("Usuário", key="username").strip()
+        st.text_input("Senha", type="password", key="password", on_change=password_entered).strip()
         st.error("😕 Usuário desconhecido ou senha incorreta.")
         return False
     else:
