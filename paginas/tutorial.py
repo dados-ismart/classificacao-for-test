e8ced72fb05f8c80a1715adb3d38dd1e234c83fe
IsmartFelipeRios <felipe.amaral@ismart.org.br>
2025-06-24 11:50:46 -0300

    colocando link do video

--- paginas/tutorial.py
+++ paginas/tutorial.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('Tutorial de uso')
 
 st.link_button('Tutorial', r'https://ismartorg-my.sharepoint.com/my?id=%2Fpersonal%2Ffelipe%5Famaral%5Fismart%5Forg%5Fbr%2FDocuments%2Ftutorial%5Fclassificacao&ga=1')
-
+st.video(r'https://ismartorg-my.sharepoint.com/personal/felipe_amaral_ismart_org_br/_layouts/15/stream.aspx?id=%2Fpersonal%2Ffelipe%5Famaral%5Fismart%5Forg%5Fbr%2FDocuments%2Ftutorial%5Fclassificacao%2Ftutorial%5Fclassificacao%5Fcoordenadoras%2Emp4&referrer=StreamWebApp%2EWeb&referrerScenario=AddressBarCopied%2Eview%2Ea56c548b%2D5133%2D4e3a%2Db1bd%2D17699da8aa02')
 st.header('Categorias de avaliação (baseadas apenas nas notas):')
 st.markdown('''
 ❌ **Crítico Escolar** - 1 nota com diferença menor que 1 ponto da média ou mais de 2 notas abaixo da média  
